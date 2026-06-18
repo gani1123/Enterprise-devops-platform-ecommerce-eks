23403c02741e2304eacab332d03edac5a8d278c0
EC2 Default User <ec2-user@ip-172-31-28-35.ec2.internal>
2026-06-18 13:19:22 +0000

    k

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,7 +1,27 @@
-dockerPush(
-    awsRegion: AWS_REGION,
-    awsCredentialsId: 'AWS-cred',
-    ecrRegistry: ECR_REGISTRY,
-    ecrRepo: ECR_REPO,
-    imageTag: IMAGE_TAG
-)
+def call() {
+
+    def awsRegion = "us-east-1"
+    def accountId = env.AWS_ACCOUNT_ID
+    def imageName = "ecommerce-api"
+    def imageTag = "1.0.0-${env.BUILD_NUMBER}"
+
+    def registry = "${accountId}.dkr.ecr.${awsRegion}.amazonaws.com"
+
+    echo "✅ Using IAM Role authentication..."
+
+    sh """
+        aws sts get-caller-identity
+
+        echo "🔐 Logging into ECR..."
+        aws ecr get-login-password --region ${awsRegion} | \
+        docker login --username AWS --password-stdin ${registry}
+
+        echo "🚀 Pushing images..."
+        docker push ${registry}/${imageName}:${imageTag}
+
+        docker tag ${registry}/${imageName}:${imageTag} ${registry}/${imageName}:latest
+
+        docker push ${registry}/${imageName}:latest
+    """
+}
+
