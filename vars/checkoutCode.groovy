5af22f0c35490616fc118e5b5b37649f8d85eed3
EC2 Default User <ec2-user@ip-172-31-28-35.ec2.internal>
2026-06-18 11:01:30 +0000

    file modified

--- vars/checkoutCode.groovy
+++ vars/checkoutCode.groovy
@@ -1,11 +1,11 @@
 def call(Map config = [:]) {
 
-```
+
 git branch: config.branch,
     credentialsId: config.credentialsId,
     url: config.repoUrl
 
 sh 'echo "Commit: $(git rev-parse --short HEAD)"'
-```
+
 
 }
